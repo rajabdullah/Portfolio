5f7fba197dce6ffa5a3015868736c52d1913fa20
Abdullah <01abdullahjaved@gmail.com>
2024-10-02 11:54:04 +0500

    Add new component Hero.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
 
       <div className="container mx-auto px-8">
         <Navbar />
+        <Hero />
       </div>
     </div>
   );
